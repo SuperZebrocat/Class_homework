a4dd684ccc32738591a20d8af4f419ba976e5d43
екатерина ермакова <katka_z_jones@mail.ru>
2024-10-11 22:46:13 +0200

    new class Product created

---
+++ src/product.py
@@ -0,0 +1,13 @@
+class Product:
+    """Класс для представления товара"""
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса"""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
