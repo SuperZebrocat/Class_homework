3abda3da5afd6751cb12efaf2915af2cd6642268
екатерина ермакова <katka_z_jones@mail.ru>
2024-10-21 22:33:21 +0200

    new functionalities for class Category added

--- src/category.py
+++ src/category.py
@@ -1,5 +1,9 @@
+from src.product import Product
+
+
 class Category:
     """Класс для представления категории товаров"""
+
     category_count = 0
     product_count = 0
 
@@ -11,6 +15,24 @@ class Category:
         """Метод для инициализации экземпляра класса"""
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product: Product):
+        """Метод добавления продуктов в категории товаров"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Свойство класса, позволяющее выводить список продуктов в виде строки"""
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб., {product.quantity} шт.\n"
+        return product_str
+
+    @property
+    def products_in_list(self):
+        """Геттер для приватного атрибута списка продуктов"""
+        return self.__products
