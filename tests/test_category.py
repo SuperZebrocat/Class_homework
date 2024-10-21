6d141caefe9d66749a75491889a29535d126659c
екатерина ермакова <katka_z_jones@mail.ru>
2024-10-21 22:38:53 +0200

    new tests for class Category added

--- tests/test_category.py
+++ tests/test_category.py
@@ -4,7 +4,7 @@ from src.category import Category
 def test_category_init(category1, category2):
     assert category1.name == "Смартфоны"
     assert category1.description == "Средство коммуникации"
-    assert len(category1.products) == 3
+    assert len(category1.products_in_list) == 3
 
     assert category1.category_count == 2
     assert category2.category_count == 2
@@ -13,3 +13,25 @@ def test_category_init(category1, category2):
     assert category1.product_count == 4
     assert category2.product_count == 4
     assert Category.product_count == 4
+
+
+def test_add_product(category1, test_product):
+    category1.add_product(test_product)
+    assert category1.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб., 5 шт.\nIphone 15, 210000.0 руб., 8 шт.\n"
+        "Xiaomi Redmi Note 11, 31000.0 руб., 14 шт.\n"
+        "Samsung Galaxy S23 Ultra, 180000.0 руб., 5 шт.\n"
+    )
+
+
+def test_products_property(category1):
+    assert category1.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб., 5 шт.\n"
+        "Iphone 15, 210000.0 руб., 8 шт.\nXiaomi Redmi Note 11, 31000.0 руб., 14 шт.\n"
+    )
+
+
+def test_products_in_list_property(category1, test_product):
+    assert len(category1.products_in_list) == 3
+    category1.add_product(test_product)
+    assert len(category1.products_in_list) == 4
