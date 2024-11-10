f623859ffa2d80dcd9913b0042680689e9c7281c
екатерина ермакова <katka_z_jones@mail.ru>
2024-11-10 20:00:26 +0200

    new tests for updeted class Category added

--- tests/test_category.py
+++ tests/test_category.py
@@ -3,44 +3,44 @@ import pytest
 from src.category import Category
 
 
-def test_category_init(category1, category2):
-    assert category1.name == "Смартфоны"
-    assert category1.description == "Средство коммуникации"
-    assert len(category1.products_in_list) == 3
+def test_category_init(test_category1, test_category2):
+    assert test_category1.name == "Смартфоны"
+    assert test_category1.description == "Средство коммуникации"
+    assert len(test_category1.products_in_list) == 3
 
-    assert category1.category_count == 2
-    assert category2.category_count == 2
+    assert test_category1.category_count == 2
+    assert test_category2.category_count == 2
     assert Category.category_count == 2
 
-    assert category1.product_count == 4
-    assert category2.product_count == 4
+    assert test_category1.product_count == 4
+    assert test_category2.product_count == 4
     assert Category.product_count == 4
 
 
-def test_add_product(category1, test_product):
-    category1.add_product(test_product)
-    assert category1.products == (
+def test_add_product(test_category1, test_product):
+    test_category1.add_product(test_product)
+    assert test_category1.products == (
         "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\nIphone 15, 210000.0 руб. Остаток: 8 шт.\n"
         "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
         "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
     )
 
 
-def test_products_property(category1):
-    assert category1.products == (
+def test_products_property(test_category1):
+    assert test_category1.products == (
         "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
         "Iphone 15, 210000.0 руб. Остаток: 8 шт.\nXiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
     )
 
 
-def test_products_in_list_property(category1, test_product):
-    assert len(category1.products_in_list) == 3
-    category1.add_product(test_product)
-    assert len(category1.products_in_list) == 4
+def test_products_in_list_property(test_category1, test_product):
+    assert len(test_category1.products_in_list) == 3
+    test_category1.add_product(test_product)
+    assert len(test_category1.products_in_list) == 4
 
 
-def test_category_str(category1):
-    assert str(category1) == "Смартфоны, количество продуктов: 27 шт."
+def test_category_str(test_category1):
+    assert str(test_category1) == "Смартфоны, количество продуктов: 27 шт."
 
 
 def test_product_iterator(product_iterator):
@@ -54,11 +54,16 @@ def test_product_iterator(product_iterator):
         next(product_iterator)
 
 
-def test_add_product_error(category1):
+def test_add_product_error(test_category1):
     with pytest.raises(TypeError):
-        category1.add_product("not a product")
+        test_category1.add_product("not a product")
 
 
-def test_add_product_smartphone(category1, test_smartphone1):
-    category1.add_product(test_smartphone1)
-    assert category1.products_in_list[-1].name == "Samsung Galaxy S23 Ultra"
+def test_add_product_smartphone(test_category1, test_smartphone1):
+    test_category1.add_product(test_smartphone1)
+    assert test_category1.products_in_list[-1].name == "Samsung Galaxy S23 Ultra"
+
+
+def test_middle_price(test_category1, test_category_empty):
+    assert test_category1.middle_price() == 140333.33
+    assert test_category_empty.middle_price() == 0
